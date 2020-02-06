869e21e004f1ab713d31778349ec95731d9ea4a0
Ádám Becsaj <becsajovszki@gmail.com>
2020-02-06 20:02:19 +0100

    done

--- Szakdolg/Migrations/202001131055237_addMunka.cs
+++ Szakdolg/Migrations/202001131055237_addMunka.cs
@@ -8,15 +8,28 @@ namespace Szakdolg.Migrations
         public override void Up()
         {
             Sql($"INSERT INTO Munkas(Nev, Img ,Elkeszult) VALUES " +
-                $"('Tetko1','tatto1.png','2018-12-10'), " +
-                $"('Tetko2','tatto1.png','2019-10-03'), " +
-                $"('Tetko3','tatto3.png','2018-07-04'), " +
-                $"('Tetko4','tatto4.png','2017-01-29'); ");
+                $"('Kastély','tetko1.png','2018-12-10'), " +
+                $"('Jel','tetko2.png','2018-07-03'), " +
+                $"('Kígyófej','tetko3.png','2017-10-14'), " +
+                $"('Felirat','tetko4.png','2017-08-26'), " +
+                $"('Szigony','tetko5.png','2018-11-09'), " +
+                $"('Rózsa','tetko6.png','2019-01-10'), " +
+                $"('Rózsa','tetko7.png','2019-01-10'), " +
+                $"('13','tetko8.png','2019-03-18'), " +
+                $"('Korona (páros)','tetko9.png','2017-02-21'), " +
+                $"('Oroszlán (páros)','tetko10.png','2017-01-16'); ");
 
             Sql($"INSERT INTO Sablonoks(Megnevezes, Img ,Ar) VALUES " +
-               $"('Sablon1','sablon1.png','2000'), " +
-               $"('Sablon2','sablon2.png','3000'), " +
-               $"('Sablon3','sablon3.png','4000'); ");
+               $"('Kígyó','minta1.png','6000'), " +
+               $"('Macska','minta2.png','5000'), " +
+               $"('Koponya','minta3.png','5000'), " +
+               $"('Ichigo','minta4.png','6000'), " +
+               $"('Csontváz','minta5.png','9000'), " +
+               $"('Csontváz','minta6.png','7000'), " +
+               $"('Joker','minta7.jpeg','10000'), " + 
+               $"('Karakter','minta8.png','15000'), " +
+               $"('Koponya','minta10.png','6000'), " +
+               $"('Joker','minta11.png','8000'); ");
 
         }
         
